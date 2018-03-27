8b833b0409782cea2a45ee7e8213e8a3e78f47d7
Jayant <jayantjnp@gmail.com>
2018-03-26 20:29:17 -0400

    lab 2 part 1 done

---
+++ Part1/Ch5/yahoo_quote_download-master/tests/test_yqd.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 18:58:59 2017
+
+@author: c0redumb
+"""
+
+from yahoo_quote_download import yqd
+
+def load_quote(ticker):
+	print('===', ticker, '===')
+	print(yqd.load_yahoo_quote(ticker, '20170515', '20170517'))
+	print(yqd.load_yahoo_quote(ticker, '20170515', '20170517', 'dividend'))
+	print(yqd.load_yahoo_quote(ticker, '20170515', '20170517', 'split'))
+
+def test():
+	# Download quote for stocks
+	load_quote('GOOGL')
+	load_quote('C')
+
+	# Download quote for index
+	load_quote('^DJI')
+
+if __name__ == '__main__':
+	test()
