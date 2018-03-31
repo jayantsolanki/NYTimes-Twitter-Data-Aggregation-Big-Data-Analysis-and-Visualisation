bcfec606dc51e396ad5440c10e2596b28dff6115
Anant Gupta <anant4158@gmail.com>
2018-03-31 12:04:09 -0400

    Add files via upload

---
+++ code/twitterDataFetch.py
@@ -0,0 +1,98 @@
+import tweepy
+import json
+import time
+import re
+import string
+from datetime import date
+from tweepy import OAuthHandler
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+sw = stopwords.words('english')
+lemma = WordNetLemmatizer()
+
+class TwitterDataCollecion(object):
+    
+    def __init__(self,query,tweetscount):
+        
+        #Initialize the twitter api connection
+        consumer_key = 'M3uDdeL5NzbjfdBGVtL5r9PZu'
+        consumer_secret = 'yF3yQnM05yuZgVSDKsaMWePs3M93f7XQh6PQHOQazyytvlMzSM'
+        access_token = '417187736-o3vz5smd7Y50QIfPxVpWdKTTA645OwRRDJhfRZBU'
+        access_secret = 'x7IbwZMcIgl29ee5uYbaHfOf5VUQh2e0rkbNFasTjk5Mq'
+         
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_secret)
+        self.api = tweepy.API(auth)
+        self.query = query
+        self.tcount = tweetscount
+
+
+    def search_tweet(self):
+        ''' Function that takes in a search string 'query', the maximum
+            number of tweets 'self.tcount'. It returns a list of 
+            tweepy.models.Status objects. '''
+     
+        searched_tweets = []
+        while len(searched_tweets) < self.tcount:
+            remaining_tweets = self.tcount - len(searched_tweets)
+            try:
+                new_tweets = self.api.search(q=self.query, count=remaining_tweets)
+                print('found',len(new_tweets),'tweets')
+                if not new_tweets:
+                    print('no tweets found')
+                    break
+                searched_tweets.extend(new_tweets)
+            except tweepy.TweepError:
+                print('exception raised, waiting 1 minutes')
+                time.sleep(1*60)
+                continue
+                #Continue the tweets collection after sleeping for 1 mins
+        return searched_tweets
+    
+    
+    def clean_text_and_tokenize(self,line):
+        line   = re.sub(r'\$\w*', '', line)
+        line   = re.sub(r'http?:.*$', '', line)
+        line   = re.sub(r'https?:.*$', '', line)
+        line   = re.sub(r'pic?.*\/\w*', '', line)
+        line   = re.sub(r'[' + string.punctuation + ']+', ' ', line)  # Remove puncutations like 's
+        
+        tokens = TweetTokenizer(strip_handles=True, reduce_len=True).tokenize(line)
+        tokens = [w.lower() for w in tokens if w not in sw and len(w) > 2 and w.isalpha()]
+        tokens = [lemma.lemmatize(word) for word in tokens]
+        
+        return tokens
+    
+#    def getCleanedWords(self,lines):
+#        words = []
+#        for line in lines:
+#            words += self.clean_text_and_tokenize(line)
+#        return words 
+       
+    def clean_tweet(self,tweet):
+        return " ".join(self.clean_text_and_tokenize(tweet))
+    
+    
+    def write_tweets(self,tweets, filename,encoding="utf-8"):
+        ''' Function that appends the cleansed tweets to a file. '''
+     
+        with open(filename, 'a') as f:
+            for tweet in tweets:
+                t = self.clean_tweet(tweet._json["text"])
+                t = str(t.encode("ascii", "ignore"))
+                f.write(t[2:-1])
+                f.write("\n")
+
+def main():
+    query = "FakeNews"
+    print("Collecting tweets for "+ query)
+    today = date.today().strftime("%d%m%y")
+    maxtweets = 5000
+    twitterclass = TwitterDataCollecion(query,maxtweets)
+    tweets=twitterclass.search_tweet()
+    twitterclass.write_tweets(tweets, "tweets/tweetsdata"+today+".txt")
+    print("Twitter Data is ready for further analysis")
+    
+if __name__ == '__main__':
+    main()
