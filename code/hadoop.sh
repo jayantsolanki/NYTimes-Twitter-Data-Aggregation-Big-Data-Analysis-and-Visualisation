8cd0b3c05869cf4b1ab1fe3837966c46f5f9e971
Jayant <jayantjnp@gmail.com>
2018-04-01 13:20:22 -0400

    added hadoop script and support ofr loc input files instead of hdfs

---
+++ code/hadoop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.8.3.jar -input input/ -output output_new2/ -mapper wordcount_mapper.py -reducer wordcount_reducer.py -numReduceTasks 1
