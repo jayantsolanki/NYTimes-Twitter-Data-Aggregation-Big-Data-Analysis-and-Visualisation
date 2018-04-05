38411be85e995934789d65248df03db08cda71f1
Anant Gupta <anant4158@gmail.com>
2018-04-04 22:18:07 -0400

    Fixed the encoding error

--- code/wordcount_mapper.py
+++ code/wordcount_mapper.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python   
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #the above just indicates to use python to intepret this file
 
 # ---------------------------------------------------------------
